f6b63d598c74fbc8f327c980f6983f38b6a48f00
lincerely <lincerely@gmail.com>
2020-04-26 22:08:46 +0800

    update lite 1.02

--- Formula/lite.rb
+++ Formula/lite.rb
@@ -1,9 +1,9 @@
 class Lite < Formula
-  version "1.01"
+  version "1.02"
   desc "A lightweight text editor written in Lua"
   homepage "https://github.com/rxi/lite"
   url "#{homepage}/archive/v#{version}.tar.gz"
-  sha256 "3150ec861ea504f131a985862193a95cfe8a59866f7078eec7c658f1e4c1045f"
+  sha256 "441068b0f0f6bd0ca42cf03e3c2197b954bd6376595b986ead70562d7b7c9141"
   head "https://github.com/rxi/lite.git"
 
   depends_on "sdl2"
