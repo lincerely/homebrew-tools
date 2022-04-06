3c08ae91f8dcb4bf2995839e63086f9fcd187ad4
lincerely <lincerely@gmail.com>
2022-04-06 16:49:21 +0800

    update inklecate to 1.0.0

--- Formula/inklecate.rb
+++ Formula/inklecate.rb
@@ -1,9 +1,9 @@
 class Inklecate < Formula
-  version "0.9.0"
+  version "1.0.0"
   desc "Commandline interpreter for inkle's open source scripting language for writing interactive narrative"
   homepage "https://github.com/inkle/ink"
-  url "#{homepage}/releases/download/#{version}/inklecate_mac.zip"
-  sha256 "ee1fd74a55df1f3c3607e9f51f5c0f9a7d9787aa52973189d085cc1b537e3dd0"
+  url "#{homepage}/releases/download/v#{version}/inklecate_mac.zip"
+  sha256 "b6f4dd1f95c180637ce193dbb5fa6d59aeafe49a2121a05b7822e6cbbaa6931f"
 
   def install
     bin.install "inklecate"
